4db4662e8a2b086400bdbc450166b65339380658
sameer-nahadiya <samnahadiya0211@gmail.com>
2025-09-18 09:37:22 +0530

    Apis

---
+++ src/index.js
@@ -0,0 +1,55 @@
+//require('dotenv').config({path: './env'}) is se better approach hai is approach ko use karne se code ke consistency bigaad the hai...
+import dotenv from "dotenv";
+
+import mongoose, { connect } from "mongoose";
+import { DB_name } from "./constants.js";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+.then(() =>{
+    app.on("error", (error) => {
+        console.log("ERRR:" , error);
+    })
+    app.listen(process.env.PORT  || 8000, () => {
+        console.log(`Serverr is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+// import express from "express";
+
+// const app = express();
+// ( async() => {
+//     try{
+//         mongoose.connect(`${process.env.MONGODB_URI}/$ {DB_name}`)
+//         app.on("error",() =>{
+//            console.log("Error",error);
+//            throw error
+//         })
+
+//         app.listen(process.env.PORT,() =>{
+//             console.log(`App is listening on port ${process.env,PORT}`);
+//         }
+
+//         )
+
+//     }
+//     catch(error){
+//         console.log("Error: ", error)
+//         throw err
+//     } 
+// })()
